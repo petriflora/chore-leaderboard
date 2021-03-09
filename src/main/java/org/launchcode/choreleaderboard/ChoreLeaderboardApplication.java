2b97ea9cafa1137e8da48abfa1c90faad1a16e61
Jennifer Morgan <jemorgaine@gmail.com>
2021-03-09 17:16:57 -0600

    - initial commit

---
+++ src/main/java/org/launchcode/choreleaderboard/ChoreLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.choreleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreLeaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/choreleaderboard/ChoreLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.choreleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
